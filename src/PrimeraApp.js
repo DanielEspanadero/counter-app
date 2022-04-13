96fa8e5c3f8733226eba9bea78beb52f18b0e953
Daniel Españadero <developer@daniel-espanadero.com>
2022-04-14 01:41:42 +0200

    defaultProps

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PrimeraApp = ( {saludo} ) => {
+const PrimeraApp = ( {saludo, subtitulo} ) => {
 
     return (
         <>
             <h1>{saludo}</h1>
-            <p>Mi primera aplicación</p>
+            <p>{subtitulo}</p>
         </>
     );
 };
 
 PrimeraApp.propTypes = {
     saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
 }
 
 export default PrimeraApp;
