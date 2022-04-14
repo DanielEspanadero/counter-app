68abece68ef7f916953abe0290bcaad23624bd08
Daniel Españadero <developer@daniel-espanadero.com>
2022-04-14 02:47:56 +0200

    finished

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -3,11 +3,19 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
 
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(value);
 
     // handleAdd
     const handleAdd = () => {
         setCounter(counter + 1)
+    };
+
+    const handleSubstract = () => {
+        setCounter(counter - 1)
+    };
+
+    const handleReset = () => {
+        setCounter(10)
     };
 
     return (
@@ -15,6 +23,8 @@ const CounterApp = ({ value }) => {
             <h1>Counter App</h1>
             <h2>{counter}</h2>
             <button onClick={handleAdd}>+1</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleSubstract}>-1</button>
         </>
     );
 };
